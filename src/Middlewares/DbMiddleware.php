4d5e608499371d8d0c0052a46ecf143fbbd28873
Mohamed Usman <emyu10@gmail.com>
2024-04-15 22:28:49 +0500

    db and jwt middlewares

--- src/Middlewares/DbMiddleware.php
+++ src/Middlewares/DbMiddleware.php
@@ -4,6 +4,9 @@ namespace Vikuraa\Middlewares;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Vikuraa\Helpers\Jwt;
+use Vikuraa\Helpers\EncryptionInterface;
+use Vikuraa\Helpers\Db;
 
 class DbMiddleware
 {
@@ -16,17 +19,27 @@ class DbMiddleware
     
     public function __invoke(Request $request, RequestHandler $handler)
     {
-        // Perform any actions before the request is handled
-        
-        // Example: Connect to the database
-        
-        // Call the next middleware or the final request handler
-        $response = $handler->handle($request);
-        
-        // Perform any actions after the request is handled
-        
-        // Example: Close the database connection
-        
-        return $response;
+        $jwt = $this->container->get(Jwt::class);
+        $userData = $jwt->getUserData($request);
+        $encryption = $this->container->get(EncryptionInterface::class);
+
+        $username = $userData->payload->username;
+        $password = $encryption->decrypt($userData->payload->password);
+
+        try {
+            $db = new Db($this->container, $username, $password);
+
+            if ($db->connected()) {
+                $this->container->set(Db::class, $db);
+                $handler->handle($request);
+            } else {
+                $response = $handler->handle($request);
+                return $response->withJson(['message' => 'Database connection failed'], 401);
+            }
+        } catch (\Exception $e) {
+            $response = $handler->handle($request);
+
+            return $response->withJson(['message' => $e->getMessage()], 401);
+        }
     }
 }
